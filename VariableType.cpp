7ff0d2fffd069d09f2991c11f6b5cfec78402601
BURLA-HARSHIT <harshithburla2006@gmail.com>
2026-02-05 21:39:26 +0530

    Create VariableType.cpp

---
+++ VariableType.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char X:
+cin>>X;
+if(X>='0'&&X<='9')
+{
+cout<<"digit";
+}
+else if(if X>='A'&&X<='Z')
+{
+cout<<"Alaphbet"<<endl;
+cout<<"Capital";
+}
+else if(X>='a'&&X<='z');
+{
+cout<<"Alphabet"<<endl;
+cout<<"small";
+}
+return 0;
+}

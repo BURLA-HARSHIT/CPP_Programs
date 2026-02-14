1c1a4c84628dd380dc71cd119150dbf13604778e
BURLA-HARSHIT <harshithburla2006@gmail.com>
2026-02-14 09:15:41 +0530

    Create Reversed order & Palindrome.cpp

---
+++ Reversed order & Palindrome.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+uisng namespace std;
+int main()
+{
+  int N,temp,rev=0;
+  cin>>N;
+  temp=N;
+  while(temp>0)
+    {
+      rev=rev*10+(temp%10);
+      temp=temo/10;
+    }
+  cout<<rev<<endl;
+  if(rev==N);
+  cout<<"Yes";
+  else";
+    return 0;
+}

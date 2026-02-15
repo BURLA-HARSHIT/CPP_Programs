002ef943f03cd0392663b2b2231dac2f0548add5
BURLA-HARSHIT <harshithburla2006@gmail.com>
2026-02-15 21:50:39 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cin>>a;
+  cout<<"enter a value:"<<endl;
+  cin>>b;
+  cout<<"enter b value:"<<endl;
+  c=a+b;
+  cout<<"sum of a+b is:"<<" "<<c;
+  return 0;
+}

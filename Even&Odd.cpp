eb5f9a727e410d86a09b5b89af34baee1d033fd3
BURLA-HARSHIT <harshithburla2006@gmail.com>
2026-02-17 18:28:06 +0530

    Create Even&Odd.cpp

---
+++ Even&Odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,id;
+  cin>>a;
+  id=a%10
+    if(id==0||id==2||id==4||id==6||id==8)
+    { 
+  cout<<a<<"is a even number"<<endl;
+}
+  else
+{
+  cout<<a<<"is a odd number"<<endl;
+}
+  return 0;
+}
+    
